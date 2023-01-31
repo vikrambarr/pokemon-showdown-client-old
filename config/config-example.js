9e1a299c224c312d8829a797c699910d9a33f97f
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-01-31 15:52:11 +0000

    maybe routed sprites and server right?

--- config/config-example.js
+++ config/config-example.js
@@ -18,8 +18,8 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'showdown',
-	host: 'sim3.psim.us',
+	id: 'pyuku',
+	host: '82.38.215.69.psim.us',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
@@ -30,5 +30,5 @@ Config.roomsFirstOpenScript = function () {
 };
 
 Config.customcolors = {
-	'zarel': 'aeo'
+	'yoshifanfic': 'aeo'
 };
