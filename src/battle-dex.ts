2fae863d20ea913d714d7ae96d3f04c573a7dcac
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-01-31 17:05:08 +0000

    still trying to highjack sprites

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -182,7 +182,7 @@ const Dex = new class implements ModdedDex {
 	resourcePrefix = (() => {
 		let prefix = '';
 		if (window.document?.location?.protocol !== 'http:') prefix = 'https:';
-		return `${prefix}//'82.38.215.69/'/`;
+		return `../`;
 		//return `${prefix}//${window.Config ? Config.routes.client : 'play.pokemonshowdown.com'}/`;
 	})();
 
