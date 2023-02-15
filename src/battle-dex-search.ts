08562c5d29da084e31c8db5c04271c40ec6c6572
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-15 22:43:54 +0000

    make hbf insurgence a thing

--- src/battle-dex-search.ts
+++ src/battle-dex-search.ts
@@ -613,7 +613,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 			this.formatType = 'letsgo';
 			this.dex = Dex.mod('gen7letsgo' as ID);
 		}
-		if (format.includes('nationaldex') || format.startsWith('nd') || format.includes('natdex')) {
+		if (format.includes('nationaldex') || format.startsWith('nd') || format.includes('natdex') || format.includes('insurgence')) {
 			format = (format.startsWith('nd') ? format.slice(2) :
 				format.includes('natdex') ? format.slice(6) : format.slice(11)) as ID;
 			this.formatType = 'natdex';
