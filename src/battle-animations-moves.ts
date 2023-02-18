2d3c169491d0af240b1878f3732bbc37cd6ce6f8
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-18 21:37:17 +0000

    link to the proper newmoon for lunarcannon

--- src/battle-animations-moves.ts
+++ src/battle-animations-moves.ts
@@ -22584,7 +22584,7 @@ export const BattleMoveAnims: AnimTable = {
 			let ystep = (defender.x - 200 - attacker.x) / 5;
 			let zstep = (defender.z - attacker.z) / 5;
 
-			scene.backgroundEffect(`url('https://${Config.routes.client}/fx/weather-newmoon.png')`, 900, 0.5);
+			scene.backgroundEffect(`url('https://raw.githubusercontent.com/vikrambarr/sprites/main/fx/weather-newmoon.png')`, 900, 0.5);
 
 			for (let i = 0; i < 5; i++) {
 				scene.showEffect('mudwisp', {
